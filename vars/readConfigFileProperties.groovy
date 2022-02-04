2f3345fa98157a166781a811205d628cc85541f1
ikekim <ik9421217@nutrien.com>
2022-02-04 01:33:43 -0500

    refactored

--- vars/readConfigFileProperties.groovy
+++ vars/readConfigFileProperties.groovy
@@ -3,6 +3,7 @@ def getConfigFileProperties(configFile) {
     yaml_container_props = null
     if (fileExists("${configFile}")) {
         yaml_container_props = readYaml file: "${configFile}"
+        sh "echo file is ${configFile}"
     }
     else {
         sh "echo No file ${configFile} exists && exit 1"
