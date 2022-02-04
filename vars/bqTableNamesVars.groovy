bae4bafda5137329d37d61cf15491fa67c3c44b0
ikekim <ik9421217@nutrien.com>
2022-02-03 23:30:09 -0500

    refactored

---
+++ vars/bqTableNamesVars.groovy
@@ -0,0 +1,10 @@
+
+def void generateTables(configfile)
+    if (fileexists("${configfile}")) {
+        yml_tables_properties = readYaml file: "${configfile}"
+    }
+    else {
+        sh "echo no files${configfile} exists && exit 1"
+    }
+
+    sh "echo yml_table_properties["project_name"]
